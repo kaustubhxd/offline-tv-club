27ecb6697f182df7c1686f4733f9c15721776c9e
itskaustubh <kaustubhpb@gmail.com>
2021-06-05 15:49:36 +0530

    deploy to github pages

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,11 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'    ? '/offlineTV/'    : '/',
     css: {
         loaderOptions: {
           sass: {
             prependData: `@import "@/assets/global.scss";`
           }
         }
-      }
-} 
-
+      },
+      productionSourceMap: false, // Previously set as true
+}
