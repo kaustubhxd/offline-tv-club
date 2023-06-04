f083e41349fa33e082127a9de653492088cfcb89
Kaustubh Bhagwat <kaustubhpb@gmail.com>
2023-06-05 01:24:23 +0530

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'    ? '/offlineTV/'    : '/',
+    publicPath: process.env.NODE_ENV === 'production'    ? '/offlinetv-club/'    : '/',
     css: {
         loaderOptions: {
           sass: {
